f3d9f78972e4ecd09a914eae11abd82fc954eb4a
iqbalgn72 <iqbalgn72@gmail.com>
2024-05-05 20:47:58 +0700

    menambahkan current and prev

--- algomateri.cpp
+++ algomateri.cpp
@@ -12,25 +12,58 @@ struct Node
 
 Node* START = NULL;
 
-void addnode() {
-        Node* newNode = new Node();
-        cout << "\nEnter the roll number of the student: ";
-        cin >> newNode->noMhs;
-        cout << "\nEnter the name of the student: ";
-        cin >> newNode-> name;\
+void addNode()
+{
+    Node * newNode = new Node();
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode -> noMhs;
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode -> name;
 
-if (START == NULL || newNode ->noMhs <= START->noMhs) {
-    if (START != NULL && newNode->noMhs == START->noMhs ){
-        cout << "/033[31mDuplicate roll numbers not allowed/033[0m" << endl;
-        return;
-    };
-    newNode->next = START;
-    if (START != NULL) {
+    if (START != NULL && newNode->noMhs == START->noMhs) {
+    
+        if (START != NULL && newNode->noMhs == START->noMhs){
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+    }
+    //jika list kosong
+        newNode->next = START;
+        if (START != NULL) 
+    {
         START->prev = newNode;
     }
-   newNode->prev =NULL;
-   START = newNode; 
+    newNode->prev = NULL;
+    START = newNode;
+    }
+    else 
+    {
+        //memasukkan new node di tengah atau di terakhir
+        Node *current = START;
+        Node *previous = NULL;
+
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+        
+        newNode->next = current;
+        newNode->prev = previous;
+
+        if (current != NULL)
+        {
+            current->prev = newNode;
+        }
+
+        if (previous != NULL)
+        {
+            previous->next = newNode;
+        }
+        else
+        {
+            //jika previous masih NULL, tandanya newnode ada di first node
+            START = newNode;
+        }
+    }
 }
-else{
-    
-}
+
