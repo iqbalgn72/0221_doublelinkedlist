2b70fd0a552ac2b6cc8a1253dd1fffcd74baa986
iqbalgn72 <iqbalgn72@gmail.com>
2024-05-05 21:00:41 +0700

    void retraverse

--- algomateri.cpp
+++ algomateri.cpp
@@ -150,6 +150,26 @@ void traverse()
     }
 }
 
+void retraverse()
+{
+    if (ListEmpty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecords in ascending order of roll number are:" << endl;
+        Node *currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+        
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+        
+    }
+}
+
 
 
 
